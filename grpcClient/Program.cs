7b2d22542e88b1d005f64e2f761990f0c3068874
cnrdmrci <canerdemirci.mail@gmail.com>
2021-03-20 15:29:06 +0300

    grpc client project created

---
+++ grpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
